b91130d6292c17c2fc66257ba1f8a6370fb62085
Jamie Isaacs <jamie@trinetcloud.com>
2015-11-14 09:09:25 -0800

    Adding rules to tests.

--- tests/Life/LifeTest.php
+++ tests/Life/LifeTest.php
@@ -9,4 +9,9 @@ class LifeTest extends \PHPUnit_Framework_TestCase
 
         $this->assertTrue($life instanceof Life);
     }
+
+    // Any live cell with fewer than two live neighbours dies, as if caused by under-population.
+    // Any live cell with two or three live neighbours lives on to the next generation.
+    // Any live cell with more than three live neighbours dies, as if by over-population.
+    // Any dead cell with exactly three live neighbours becomes a live cell, as if by reproduction.
 }
