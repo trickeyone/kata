86ea405c6e40cbca3add1f84aa1c817dbe9e597e
Jamie Isaacs <jamie@trinetcloud.com>
2016-04-14 10:42:06 -0700

    Updating currency conversion.

--- src/CurrencyConversion/README.md
+++ src/CurrencyConversion/README.md
@@ -2,5 +2,6 @@
 
 ## Rules
 
-1) Given 10 USD (U.S. Dollar) convert to 8 EUR (Euro) using 0.8 rate
-2) Given 396.5 RUB (Russian Ruble) convert to 4.23 GBP (British Pound) using 0.011 exchange rate
+1) Given 10 USD (U.S. Dollar) convert to 8 EUR (Euro) using 0.8 exchange rate
+2) Given 30 CHF (Swiss Franc) convert to 3411 JPY (Japanese Yen) using 113.7 exchange rate
+3) Given 396.5 RUB (Russian Ruble) convert to 4.3615 GBP (British Pound) using 0.011 exchange rate
--- tests/CurrencyConversion/CurrencyConversionTest.php
+++ tests/CurrencyConversion/CurrencyConversionTest.php
@@ -3,10 +3,27 @@ namespace pdt256\kata\CurrencyConversion;
 
 class CurrencyConversionTest extends \PHPUnit_Framework_TestCase
 {
-    public function testSetup()
-    {
-        $currencyConversion = new CurrencyConversion;
+    /** @var CurrencyConversion */
+    protected $currencyConversion;
 
-        $this->assertTrue($currencyConversion instanceof CurrencyConversion);
+    protected function setUp()
+    {
+        parent::setUp();
+        $this->currencyConversion = new CurrencyConversion();
+    }
+
+    // 1) Given 10 USD (U.S. Dollar) convert to 8 EUR (Euro) using 0.8 rate
+    public function testConvertUSDtoEUR()
+    {
+    }
+
+    // 2) Given 30 CHF (Swiss Franc) convert to 3411 JPY (Japanese Yen) using 113.7 exchange rate
+    public function testConvertCHFtoJPY()
+    {
+    }
+
+    // 3) Given 396.5 RUB (Russian Ruble) convert to 4.3615 GBP (British Pound) using 0.011 exchange rate
+    public function testConvertRUBtoGBP()
+    {
     }
 }
